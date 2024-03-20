383db28dbc8ad4227ed4d617e9beeba44e957967
cmkbeew <cmkbeew@naver.com>
2024-03-20 21:18:46 +0900

    DB 연동, 회원가입(일반, 기업) 기능 추가

---
+++ okky/src/main/java/member/MemberDTO.java
@@ -0,0 +1,103 @@
+package member;
+
+public class MemberDTO {
+	private int idx;
+	private String memberId;
+	private String pwd;
+	private String email;
+	private String name;
+	private String nickname;
+	private String tags;
+	private String type;
+	private String companyName;
+	private String companyNumber;
+	private String orgCompanyFile;
+	private String saveCompanyFile;	
+	private String managerName;
+	private String managerPhone;
+	
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getTags() {
+		return tags;
+	}
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getCompanyNumber() {
+		return companyNumber;
+	}
+	public void setCompanyNumber(String companyNumber) {
+		this.companyNumber = companyNumber;
+	}
+	public String getOrgCompanyFile() {
+		return orgCompanyFile;
+	}
+	public void setOrgCompanyFile(String orgCompanyFile) {
+		this.orgCompanyFile = orgCompanyFile;
+	}
+	public String getSaveCompanyFile() {
+		return saveCompanyFile;
+	}
+	public void setSaveCompanyFile(String saveCompanyFile) {
+		this.saveCompanyFile = saveCompanyFile;
+	}
+	public String getManagerName() {
+		return managerName;
+	}
+	public void setManagerName(String managerName) {
+		this.managerName = managerName;
+	}
+	public String getManagerPhone() {
+		return managerPhone;
+	}
+	public void setManagerPhone(String managerPhone) {
+		this.managerPhone = managerPhone;
+	}
+}
