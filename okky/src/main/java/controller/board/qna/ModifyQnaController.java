c82f2c8485fcbb6e21b2947832a66bd5b6a4b073
Ryurim <ryurim1996@gmail.com>
2024-03-25 18:09:22 +0900

    메인에 마지막 푸시

--- okky/src/main/java/controller/board/qna/ModifyQnaController.java
+++ okky/src/main/java/controller/board/qna/ModifyQnaController.java
@@ -119,7 +119,7 @@ public class ModifyQnaController extends HttpServlet {
 			PrintWriter writer = response.getWriter();
 			writer.println("<script>");
 			writer.println("alert('글 수정 시 에러가 발생하였습니다.')");
-			writer.println("window.location.replace('list.do')");
+			writer.println("window.location.replace('/okky/main.do')");
 			writer.println("</script>");
 		}
 	}
